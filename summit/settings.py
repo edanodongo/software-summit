45daa982304f3886f9e1c16e7fc0314890c6ad14
Edan Odongo <edanodongo05@gmail.com>
2025-08-27 16:20:45 +0300

    extra images

--- summit/settings.py
+++ summit/settings.py
@@ -115,6 +115,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+# Tell Django to look for static files both inside the app and in the global static folder
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Only used in production (when running `collectstatic`)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
