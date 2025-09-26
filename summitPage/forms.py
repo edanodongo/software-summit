54759f6b5f4ac61d653cc82550fcdc11ae879d2f
Edan Odongo <code_genius@debian.local>
2025-09-26 12:44:51 +0300

    dashboard updates

--- summitPage/forms.py
+++ summitPage/forms.py
@@ -6,19 +6,16 @@ class QuickRegistrationForm(forms.ModelForm):
     interests = forms.MultipleChoiceField(
         choices=Registrant.INTEREST_CHOICES,
         widget=forms.CheckboxSelectMultiple,
-        required=False,
     )
     updates_opt_in = forms.BooleanField(required=False)
 
     other_organization_type = forms.CharField(
-        required=False,
         widget=forms.TextInput(attrs={
             'class': 'form-control mt-2',
             'placeholder': 'Please specify...',
         })
     )
     other_interest = forms.CharField(
-        required=False,
         widget=forms.TextInput(attrs={
             'class': 'form-control mt-2',
             'placeholder': 'Please specify...',
