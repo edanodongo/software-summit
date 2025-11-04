86aeb94d16520b3c23241e3d48ce255df7fab853
sylvester976 <officialotienosylvester@gmail.com>
2025-11-04 18:12:09 +0300

    test to pass

--- summit/settings.py
+++ summit/settings.py
@@ -30,7 +30,7 @@ else:
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['DEBUG']
+DEBUG = config.get('DEBUG', True)
 
 ALLOWED_HOSTS = config['ALLOWED_HOSTS']
 
